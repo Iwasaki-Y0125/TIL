f7cacde099cce7dd6d472a25881c6d8ec227d55a
Iwasaki Yuka <dcmmw012@gmail.com>
2025-07-01 15:08:37 +0900

    ruby_workspaceをtil内へ移動

---
+++ ruby_workspace/hello.rb
@@ -0,0 +1,5 @@
+def greet
+puts " Hello, World!"
+end
+
+5.times {greet}
---
+++ ruby_workspace/sample.rb
@@ -0,0 +1,20 @@
+def greet(message)
+    puts message
+end
+greet("Hello, World!")
+
+def add(a , b)
+    return a + b
+end
+puts add(3 , 4)
+
+def multiply(a , b)
+    return a * b
+end
+puts multiply(3, 4)
+
+def square(num)
+    return num * num
+end
+puts square(5)
+
