2729ecb1a8cb929659b0ebb0d1bccf172709f7be
Iwasaki Yuka <dcmmw012@gmail.com>
2025-07-18 17:17:23 +0900

    Ruby入門Chap1,2

---
+++ ruby_walking/chapter2/constant.rb
@@ -0,0 +1,5 @@
+PI = 3.14
+TAX_RATE = 0.1
+
+puts PI
+puts TAX_RATE
---
+++ ruby_walking/chapter2/string.rb
@@ -0,0 +1,11 @@
+puts 'Hello'
+puts 'Hello ' + 'RUNTEQ'
+puts 'Hello' + ' ' + 'RUNTEQ'
+
+name = "Ruby"
+puts 'シングルクォート：Hello,#{name}'
+puts "ダブルクォート：Hello,#{name}"
+
+puts 1 + 1
+puts '1' + '1'
+puts '1'.to_i + '1'.to_i
